b3074866f83866ec0da3b573ba39e14c8624e154
1234567890 <phantiensy195@gmail.com>
2018-07-06 10:07:33 +0700

    MyLinkedListQueue trien khai

---
+++ src/main/java/thuchanh/trienkhai/Queue/thongquaLinkedList/Node.java
@@ -0,0 +1,11 @@
+package thuchanh.trienkhai.Queue.thongquaLinkedList;
+
+class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
